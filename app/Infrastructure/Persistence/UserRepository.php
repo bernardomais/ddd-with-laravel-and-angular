3b12e8c8e4d1a8563469877d5853b44c622aaaa3
Bernardo Marambaia <marambaiabernardo@gmail.com>
2024-07-02 00:30:14 -0300

    Create repository classes in the infrastructure layer, to persist and fetch data from the database.

---
+++ app/Infrastructure/Persistence/UserRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Infrastructure\Persistence;
+
+use App\Models\User;
+
+class UserRepository implements UserRepositoryInterface
+{
+    public function create(array $data): User
+    {
+        return User::create($data);
+    }
+
+    public function findByEmail(string $email): ?User
+    {
+        return User::where('email', $email)->first();
+    }
+
+    public function findById(int $id): ?User
+    {
+        return User::find($id);
+    }
+}
